beeb7e8649dac64289712aa0908c2b29992a6a63
Clara Teodoro <teodoroclara13@gmail.com>
2024-06-28 01:40:47 +0200

    segundo commit

--- exercicios/para-casa/task2.py
+++ exercicios/para-casa/task2.py
@@ -12,3 +12,5 @@ print("\n")
 print("Números de 1 a 20, um ao lado do outro:")
 for i in range(1, 21):
     print(i, end=' ')
+
+print("\n")
--- exercicios/para-casa/task3.py
+++ exercicios/para-casa/task3.py
@@ -1,18 +1,16 @@
 # * Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. 
 # O usuário deverá informar a quantidade de CDs e o valor para em cada um.
 
-quant_cds = int(input("Informe a quantidade de CDs na coleção: "))
-valor_total_investido = 0.0
+cd_values = []
 
-for i in range(quant_cds):
-        valor_cd = float(input(f"Informe o valor pago pelo CD {i+1}: "))
-        valor_total_investido += valor_cdif quant_cds > 0:
-    valor_medio = valor_total_investido / quantidade_cds
-else:
-valor_medio = 0.0
-    
-print(f"\nValor total investido: R$ {valor_total_investido:.2f}")
-print(f"Valor médio gasto por CD: R$ {valor_medio:.2f}")
+quant_cds = int(input("Informe a quantidade total de CDs: "))
+total_value = 0.0
 
-if __name__ == "__main__":
-    main()
+for cd_value in range (quant_cds):
+    cd_value = float(input("Informe o valor deste CD: "))
+    cd_values.append (cd_value)
+    total_value = sum(cd_values)
+    average_value = total_value / quant_cds
+
+print (f"Valores investidos em cada CD: {cd_values}")
+print (f"Valor médio por CD considerando a quantitade de {quant_cds} CDs e o valor total de R$ {total_value}: R$ {average_value}")
