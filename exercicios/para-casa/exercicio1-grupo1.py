55283de16ed9888902c74605a53770536781cbda
Sueide Vilela <sueide.t@gmail.com>
2024-06-27 20:34:01 -0300

    exercicio

--- exercicios/para-casa/exercicio1-grupo1.py
+++ exercicios/para-casa/exercicio1-grupo1.py
@@ -2,10 +2,12 @@ eleitores = int(input("Digite o número de eleitores: "))
 votos = []
 
 for eleitor in range(eleitores):
-    voto = int(input(f"Eleitor {eleitor+1} Digite o número do candidato (1 em diante): "))
+    voto = int(input(f"Eleitor {eleitor+1} Digite o número do candidato: "))
     if voto > len(votos):
         votos += [0] * (voto - len(votos))
     votos[voto - 1] += 1
 
-for candidata, votos_candidata in enumerate(votos):
-    print(f"Candidata {candidata + 1}: {votos_candidata} voto(s)")
+candidata = 1
+for votos_candidata in votos:
+    print(f"Candidata {candidata}: {votos_candidata} voto(s)")
+    candidata += 1
