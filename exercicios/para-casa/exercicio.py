8a14b849dcb0cba0882aad04ece65169e01c5a8a
Xainã França <xaifran5@gmail.com>
2024-06-27 01:28:53 -0300

    Exercicio2

---
+++ exercicios/para-casa/exercicio.py
@@ -0,0 +1,23 @@
+#* Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
+
+candidato_1 = "Luis"
+candidato_2 = "Gustavo"
+candidato_3 = "Douglas"
+
+
+
+
+
+#* Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+
+iterador = 0
+
+for iterador in range (1, 21):
+    print(iterador)
+
+for iterador in range (1,21):
+    print(iterador, end=" ")
+
+
+#* Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um.
