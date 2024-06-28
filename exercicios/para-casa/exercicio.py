2931067260aef78419a6c90f01afd5a544554295
Xainã França <xaifran5@gmail.com>
2024-06-27 22:11:57 -0300

    todos os exercicios feitos

--- exercicios/para-casa/exercicio.py
+++ exercicios/para-casa/exercicio.py
@@ -1,15 +1,30 @@
 #* Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
 
-candidato_1 = "Luis"
-candidato_2 = "Gustavo"
-candidato_3 = "Douglas"
+votos_candidato1 = 0
+votos_candidato2 = 0
+votos_candidato3 = 0
 
+numero_eleitores = int(input("Quantos eleitores tem essa eleição?"))
+
+for eleicao in range (0, numero_eleitores):
+    voto = int(input("Você quer votar no cadidato1, candidato2 ou candidato3? Responda com o número."))
+    if voto == 1:
+        votos_candidato1 += 1
+    elif voto == 2:
+        votos_candidato2 += 1
+    elif voto == 3:
+        votos_candidato3 += 1
+    else:
+        print("Seu voto é inválido")
+        voto = int(input("Você quer votar no cadidato1, candidato2 ou candidato3? Responda com o número."))
+
+print(f"O candidato 1 teve {votos_candidato1}, o candidato 2 teve {votos_candidato2} e o candidato 3 teve {votos_candidato3}")
+  
 
 
 
 
 #* Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
-
 
 iterador = 0
 
@@ -20,4 +35,19 @@ for iterador in range (1,21):
     print(iterador, end=" ")
 
 
+
+
 #* Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um.
+
+total_cds = int(input("Luisa, você tem uma coleção bem legal de CDs, não é? Tu sabe quantos CDs são ao total? "))
+valor = []
+
+for valor_cds in range (0, total_cds):
+    valor_unidade = float(input("Olha esse CD. Quanto custou esse? "))
+    valor.append(valor_unidade)
+    valor_investido = sum(valor)
+    media_investimento = valor_investido / len(valor)
+
+print(f"Ah, então você investiu o total de R${valor_investido} e a média de invetimento é de R${media_investimento} por CD.")
+
+
