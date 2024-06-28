abe513856ef9da14c02813a0f8c4ee752d0c1920
Silvia RF <silvia.follador@gmail.com>
2024-06-27 21:04:11 -0400

    exercícios revistos no grupo de estudos

--- exercicios/para-casa/exercicio_1.3.py
+++ exercicios/para-casa/exercicio_1.3.py
@@ -5,16 +5,17 @@
 # quantidade total gasta
 # média do valor gasto
 
+valores_cd = []
 quantidade_cds = int(input ("Informe quantos cds você possui: "))
-colecao_cds = {}
 
-for cd in range (quantidade_cds):
-    valor = float(input(f"Informe o valor do CD {cd + 1}: "))
-    colecao_cds[F"CD {cd + 1}"] = {"Valor": valor}
+for valor_cd in range (quantidade_cds):
+    valor_cd = float(input("Digite o valor de cada CD, um por vez: "))
+    valores_cd.append (valor_cd)
+    valor_total_investido = sum(valores_cd)
+    valor_medio = valor_total_investido / quantidade_cds
 
-print("Coleção de CDs: ")
-for cd, info in colecao_cds.items():
-    print(f"{cd}: R${info["Valor"]:.2f}")
+    print (f"Valores por cada CD: {valores_cd}")
+    print (f"O valor total gasto é de {valor_total_investido} e o valor médio de cada CD é de {valor_medio}")
 
 
 
